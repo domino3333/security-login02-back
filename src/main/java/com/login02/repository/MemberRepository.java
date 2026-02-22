008cf4202dcdc87dfe0f1990538833ec00d18979
Minho Kang <busster@naver.com>
2026-02-22 17:12:55 +0900

    member Repository를 권한도 같이 가져오게 변경 / 그냥 findbyemail만 써버리면 memberRoleSet이 현재 lazy타입이기때문에 한번에 가져오지 못함. 그래서 entitygraph로 한번에 가져오도록 한다.

--- src/main/java/com/login02/repository/MemberRepository.java
+++ src/main/java/com/login02/repository/MemberRepository.java
@@ -2,12 +2,17 @@ package com.login02.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.login02.domain.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long>{
 
-    Optional<Member> findByEmail(String email);
+    @EntityGraph(attributePaths = { "memberRoleSet" })
+	@Query("select m from Member m where m.email = :email")
+	Optional<Member> getWithRoles(@Param("email") String email);
 
 }
