f649b989cef29fe20f51e941a4659066bd64822b
Minho Kang <busster@naver.com>
2026-02-22 17:13:18 +0900

    멤버 객체 테이블 매핑

---
+++ src/main/java/com/login02/domain/Member.java
@@ -0,0 +1,75 @@
+package com.login02.domain;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "MEMBER")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq")
+    @SequenceGenerator(
+            name = "member_seq",
+            sequenceName = "member_seq",
+            allocationSize = 1
+    )
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "EMAIL", nullable = false, unique = true, length = 50)
+    private String email;
+
+    @Column(name = "PASSWORD", nullable = false, length = 100)
+    private String password;
+
+    @Column(name = "CREATED_AT", updatable = false)
+    private LocalDateTime createdAt;
+
+    @ElementCollection(fetch = FetchType.LAZY)
+    @CollectionTable(
+            name = "MEMBER_ROLE",
+            joinColumns = @JoinColumn(name = "MEMBER_ID")
+    )
+    @Column(name = "ROLE", length = 20)
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    private Set<MemberRole> memberRoleSet = new HashSet<>();
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public void addRole(MemberRole role) {
+        this.memberRoleSet.add(role);
+    }
+    
+    
+}
